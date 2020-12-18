917e227d7b9ca0214a364cfb4fffd76f1e788058
Daniel Tedenljung <info@tedenljungconsulting.com>
2020-12-18 16:40:51 +0100

    My settings

--- main/configuration.h
+++ main/configuration.h
@@ -6,6 +6,7 @@ Copyright (C) 2018 by Xose Pérez <xose dot perez at gmail dot com>
 
 This code requires LMIC library by Matthijs Kooijman
 https://github.com/matthijskooijman/arduino-lmic
+https://github.com/mcci-catena
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -32,8 +33,8 @@ void ttn_register(void (*callback)(uint8_t message));
 // Version
 // -----------------------------------------------------------------------------
 
-#define APP_NAME                "TTN MAP-TRACK"
-#define APP_VERSION             "1.1.0"
+#define APP_NAME                "TCAB TTN-Tracker"
+#define APP_VERSION             "1.2.0"
 
 // -----------------------------------------------------------------------------
 // Configuration
@@ -58,17 +59,21 @@ void ttn_register(void (*callback)(uint8_t message));
 #define DEBUG_PORT              Serial      // Serial debug port
 #define SERIAL_BAUD             115200      // Serial debug baud rate
 #define SLEEP_BETWEEN_MESSAGES  true        // Do sleep between messages
-#define SEND_INTERVAL           (3 * 60 * 1000) // Sleep for these many millis
-#define MESSAGE_TO_SLEEP_DELAY  5000        // Time after message before going to sleep
+#define SEND_INTERVAL           (1 * 60 * 1000) // Sleep for these many millis
+#define MESSAGE_TO_SLEEP_DELAY  100        // Time after message before going to sleep
 #define LOGO_DELAY              5000        // Time to show logo on first boot
 #define LORAWAN_PORT            1          // Port the messages will be sent to
-#define LORAWAN_CONFIRMED_EVERY 10          // Send confirmed message every these many messages (0 means never)
+#define LORAWAN_CONFIRMED_EVERY 0          // Send confirmed message every these many messages (0 means never)
 #define LORAWAN_SF              DR_SF7      // Spreading factor (recommended DR_SF7 for ttn network map purposes, DR_SF10 works for slow moving trackers)
 #define LORAWAN_ADR             0           // Enable ADR
 #define REQUIRE_RADIO           true        // If true, we will fail to start if the radio is not found
+#define GPS_SLEEP               false       // Put GPS to sleep
+#define LORA_SLEEP              true        // Put LoRa to sleep
+#define OLED_WAKEUP             true        // Wake OLED up after sleep              
+
 
 // If not defined, we will wait for lock forever
-#define GPS_WAIT_FOR_LOCK       (2 * 60 * 1000)  // Wait after every boot for GPS lock (may need longer than 5s because we turned the gps off during deep sleep)
+#define GPS_WAIT_FOR_LOCK       (1 * 60 * 1000)  // Wait after every boot for GPS lock (may need longer than 5s because we turned the gps off during deep sleep)
 
 // -----------------------------------------------------------------------------
 // DEBUG
